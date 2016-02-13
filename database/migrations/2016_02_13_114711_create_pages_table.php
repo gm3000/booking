029b157c7235cef711ba3f1bdc4521079bc0fe35
perrywang <perrywangsh@gmail.com>
2016-02-13 20:18:44 +0800

    add pages table migration

--- database/migrations/2016_02_13_114711_create_pages_table.php
+++ database/migrations/2016_02_13_114711_create_pages_table.php
@@ -15,6 +15,10 @@ class CreatePagesTable extends Migration
         Schema::create('pages', function (Blueprint $table) {
             $table->increments('id');
             $table->string('slug');
+            $table->string('title_en');
+            $table->string('title_cn');
+            $table->string('content_en');
+            $table->string('content_cn');
             $table->softDeletes();
             $table->timestamps();
         });
