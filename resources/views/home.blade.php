508e386ee806c8a61e7e3674486a3890af88d45d
Ryan Huang <hhao3000@gmail.com>
2016-02-16 01:24:50 +0800

    add hot places gallery in home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,6 +4,8 @@
 @section('styles')
     @parent
     <link rel="stylesheet" type="text/css" href="/css/lightslider.min.css">
+    <link rel="stylesheet" href="/css/nanoGALLERY/nanogallery.min.css">
+    <link rel="stylesheet" href="/css/nanoGALLERY/themes/light/nanogallery_light.min.css">
     <style>
         .home-slide {
             display: block;
@@ -11,6 +13,10 @@
             background-color: #00A6C7;
             background-size: cover;
             background-position: center center;
+        }
+
+        #home-detail{
+            margin-top: 5rem;
         }
     </style>
 @endsection
@@ -29,12 +35,27 @@
         </li>
     </ul>
 </div>
+<div id="home-detail">
+    <div class="ui grid container">
+        <div class="eleven wide column">
+            <div id="home-gallery"></div>
+        </div>
+        <div class="five wide column">
+            <div class="ui segment">
+                xixixixi
+            </div>
+        </div>
+    </div>
+</div>
+
+
 
 @endsection
 
 @section('scripts')
 	@parent
     <script src="/js/lightslider.min.js"></script>
+    <script src="/js/jquery.nanogallery.min.js"></script>
 	<script type="text/javascript">
 		$('.menu .item').removeClass('active');
 		$('#home').addClass('active');
@@ -50,6 +71,77 @@
                     speed: 800,
 
                 }
+        );
+
+        $('#home-gallery').nanoGallery(
+
+                {
+                    thumbnailWidth: 'auto',
+                    thumbnailHeight: 250,
+
+                    theme: "light",
+
+                    //thumbnailAlignment: "justified",
+                    thumbnailGutterWidth : 0,
+                    thumbnailGutterHeight : 0,
+                    thumbnailHoverEffect: [{ name: 'labelAppear75', duration: 300 }],
+                    thumbnailLabel: {
+                                        display: true,
+                                        position: 'overImageOnMiddle',
+                                        align: 'center',
+                                        hideIcons: true,
+                                    },
+                    fnThumbnailOpen: function(items){
+                        window.location.href=items[0].src;
+                    },
+                    items: [
+                        {
+                            src:'/about',
+                            srct:'/images/thumbnails/LasVegas.jpg',
+                            title: "LasVegas"
+                        },
+                        {
+                            src: '/about',
+                            srct: '/images/thumbnails/newyork.jpg',
+                            title: 'NewYork'
+                        },
+                        {
+                            src: '/about',
+                            srct: '/images/thumbnails/london.jpg',
+                            title: 'London'
+                        },
+                        {
+                            src: '/about',
+                            srct: '/images/thumbnails/chicago.jpg',
+                            title: 'Chicago'
+                        },
+                        {
+                            src: '/about',
+                            srct: '/images/thumbnails/paris.jpg',
+                            title: 'Paris'
+                        },
+                        {
+                            src: '/about',
+                            srct: '/images/thumbnails/tokyo.jpg',
+                            title: 'Tokyo'
+                        },
+                        {
+                            src: '/about',
+                            srct: '/images/thumbnails/orlando.jpg',
+                            title: 'Orlando'
+                        },
+                        {
+                            src: '/about',
+                            srct: '/images/thumbnails/sydney.jpg',
+                            title: 'Sydney'
+                        },
+                        {
+                            src: '/about',
+                            srct: '/images/thumbnails/losangeles.jpg',
+                            title: 'Los Angeles (LA)'
+                        }]
+                }
+
         );
 
 	</script>
