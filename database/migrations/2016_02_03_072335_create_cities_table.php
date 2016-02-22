1bc8e9d315e53b2713ab03790918c7c77667f562
Ryan Huang <hhao3000@gmail.com>
2016-02-22 11:49:45 +0800

    add heat field to city table

--- database/migrations/2016_02_03_072335_create_cities_table.php
+++ database/migrations/2016_02_03_072335_create_cities_table.php
@@ -15,6 +15,7 @@ class CreateCitiesTable extends Migration
         Schema::create('cities', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('country_id');
+            $table->integer('heat');
             $table->string('name_cn');
             $table->string('name_en');
             $table->text('desc_cn');
