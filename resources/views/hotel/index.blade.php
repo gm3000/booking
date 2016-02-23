10151eb09ad05519511af4d7370e67e8cf84c52a
Perry Wang <perrywangsh@gmail.com>
2016-02-23 11:25:44 +0800

    modify hotels list page layout

--- resources/views/hotel/index.blade.php
+++ resources/views/hotel/index.blade.php
@@ -5,22 +5,22 @@
 @include('partial.slider',['images'=>['/images/slider/travel1.jpg','/images/slider/travel2.jpg','/images/slider/travel3.jpg']])
 <div id="hotel_list" style="padding-top:3em;">
   <div class="ui grid container">
-    <div class="ui three column row">
-      <div class="ui column">
+    <div class="ui row">
+      <div class="ui four wide column">
         <h1 class="ui red header" style="display:inline-block">
           全球星级酒店
         </h1>
       </div>
-      <div class="ui right aligned column">
+      <div class="ui seven wide left aligned column">
         <div id="city_list" class="ui horizontal list" style="font-weight:bold;">
           @foreach($cities as $city)
             <a class="item" href="#">{{$city->name}}</a>
           @endforeach
         </div>
       </div>
-      <div class="ui right aligned column">
+      <div class="ui five wide right aligned column">
         <form id="searchForm" class="" action="/hotels/search" method="get" style="display:inline;">
-          <div class="ui icon input" style="width:21em;">
+          <div class="ui fluid icon input">
             <input type="text" name="query" value="{{ Request::input('query') }}" placeholder="{{ trans('site.search') }}">
             <i id="search" class="circular search link icon"></i>
             <input type="submit" style="display:none;">
