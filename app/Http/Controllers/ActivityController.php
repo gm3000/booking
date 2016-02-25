86de8fba2b2963213d893ca617af86acfeb53535
perrywang <perrywangsh@gmail.com>
2016-02-25 22:02:49 +0800

    search activity function defect fix

--- app/Http/Controllers/ActivityController.php
+++ app/Http/Controllers/ActivityController.php
@@ -18,7 +18,7 @@ class ActivityController extends Controller
     public function index()
     {
       $lang = \App::getLocale();
-      $activities = Activity::orderBy('name')->paginate(15,['id','name_'.$lang.' as name','desc_'.$lang.' as desc']);
+      $activities = Activity::orderBy('name')->paginate(15,['id','name_'.$lang.' as name','desc_'.$lang.' as desc','poster']);
       return view('activity.index',compact('activities'));
     }
 
